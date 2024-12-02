2188a980d8e15e74f024310b6685e4dfbe0f8357
Nick Werginz <nick.werginz@lernende.bbw.ch>
2024-12-02 16:26:12 +0100

    first commit

---
+++ src/main/java/com/example/cypresstutorialbackend/CypressTutorialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cypresstutorialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypressTutorialBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CypressTutorialBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cypresstutorialbackend/CypressTutorialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cypresstutorialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypressTutorialBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
